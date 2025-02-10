fd12c7087cc802e5aeb537131b27273cf1fb1eb7
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-02-10 12:18:49 +0600

    crime reports page basic design

--- backENd/server.js
+++ backENd/server.js
@@ -1,14 +1,15 @@
-const express = require("express"); // Only import express here
+const express = require("express"); 
 const cors = require("cors");
 const sql = require("mssql");
 require("dotenv").config();
+
 const bodyParser = require("body-parser");
-const authRoutes = require("./routes/auth"); // Import authRoutes here
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173",  // Allow requests from the frontend URL
+    origin: "http://localhost:5173", 
     methods: "GET,POST",
     credentials: true,
   };
@@ -16,9 +17,8 @@ const corsOptions = {
   app.use(cors(corsOptions));
   
 app.use(express.json());
-app.use("/auth", authRoutes);  // Use the authRoutes for handling authentication
+app.use("/auth", authRoutes); 
 
-// Database connection
 const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -30,15 +30,12 @@ const dbConfig = {
     }
 };
 
-// Connect to SQL Server
 sql.connect(dbConfig)
     .then(() => console.log("Connected to SQL Server"))
     .catch(err => console.error("Database Connection Failed", err));
 
-// Test route
 app.get("/", (req, res) => {
     res.send("API is working!");
 });
 
-// Start the server
 app.listen(3000, () => console.log("Server running on port 5000"));
--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import CrimeReports from './components/crimeReports';
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                 <Routes>
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/crime-reports" element={<CrimeReports />} />
                 </Routes>
             </div>
         </Router>
