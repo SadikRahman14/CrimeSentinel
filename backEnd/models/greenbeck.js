da4dacef717ad153d923fe76714041992f914476
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-02-23 12:00:28 +0600

    basic sql query

--- backEnd/models/greenbeck.js
+++ backEnd/models/greenbeck.js
@@ -23,11 +23,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,  
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false, 
-      unique: true,      
-    }
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+        unique: true,      
+      }
   }, {
     sequelize,
     modelName: 'GreenBeck',
